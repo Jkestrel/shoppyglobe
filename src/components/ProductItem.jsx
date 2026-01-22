a742cc6bd314d11eb7bd3dc759f09436960793cf
Christo <kestreljack7@gmail.com>
2026-01-22 23:31:51 +0530

    ProductItem added

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./ProductItem.css";
+
+function ProductItem({ product }) {
+  return (
+    <div className="product-card">
+      <img
+        src={product.thumbnail}
+        alt={product.title}
+        loading="lazy"
+      />
+
+      <h3>{product.title}</h3>
+      <p>₹{product.price}</p>
+
+      <Link to={`/product/${product.id}`}>
+        <button>View Details</button>
+      </Link>
+    </div>
+  );
+}
+
+export default ProductItem;
