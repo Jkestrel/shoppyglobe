76ba291590fd43a8d2addf26fcb488cb433098f8
Christo <kestreljack7@gmail.com>
2026-01-22 22:50:42 +0530

    main and app.jsx added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { RouterProvider } from "react-router-dom";
-// import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import { store } from "./redux/store";
-// import router from "./routes/router";
+import router from "./routes/router";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
