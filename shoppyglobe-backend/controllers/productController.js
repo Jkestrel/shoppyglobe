4fa57749f1e9b6d40c2e9cffd0d4f94a1cd212be
Christo <kestreljack7@gmail.com>
2026-01-24 19:05:19 +0530

    product controller

---
+++ shoppyglobe-backend/controllers/productController.js
@@ -0,0 +1,26 @@
+import Product from "../models/Product.js";
+
+// GET /products
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// GET /products/:id
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Invalid product ID" });
+  }
+};
